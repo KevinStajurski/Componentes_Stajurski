d4cdf65d313e7f48844199220a81ce1081852522
Kevin Stajurski <kevinstajurski@gmail.com>
2024-03-19 00:05:43 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SideMenuComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
